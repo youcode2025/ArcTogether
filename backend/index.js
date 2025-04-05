5a8b41e087c9f8f375ab42278bb66ab2ab19ce13
Ruiyang Zhang <ruiyangz57@gmail.com>
2025-04-05 15:13:50 -0700

    basic backend structure

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // User routes
 router.use('/api/users', require('./users'));
-router.use('/api/activities', require('./activities'));
+router.use('/api/activities', require('./src/routes/activities'));
 router.use('/api/skills', require('./skills'));
 router.use('/api/points', require('./points'));
 router.use('/api/photos', require('./photos'));
